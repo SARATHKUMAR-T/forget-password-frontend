35e35a17154af8de5f122e1425f68b98884ef931
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-06-25 12:29:21 +0530

    all

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
     <div className="App max-w-full min-h-screen">
       <Routes>
         <Route exact path="/" element={<Login />} />
-        <Route  path="/signup" element={<Signup />} />
-        <Route  path="/forgot-password" element={<Forgot />} />
-        <Route  path="/reset-password/:id/:token" element={<ForgetLanding />} />
-        <Route  path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<Forgot />} />
+        <Route path="/reset-password/:id/:token" element={<ForgetLanding />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -80,7 +80,7 @@ const Signup = () => {
               </label>
               <input
                 type="text"
-                id="name"
+                id="lastName"
                 value={lastName}
                 onChange={e => setLastName(e.target.value)}
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
@@ -129,7 +129,7 @@ const Signup = () => {
                 type="password"
                 id="repeat-password"
                 value={repeatpassword}
-                onChange={(e)=>setRepeatPassword(e.target.value)}
+                onChange={e => setRepeatPassword(e.target.value)}
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
               />
             </div>
